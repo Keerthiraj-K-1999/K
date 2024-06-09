2badcf0b320f9ea622309abad8d15bfa744d1f7f
Keerthiraj-K-1999 <keerthirajkumaravel@gmail.com>
2024-06-09 21:29:08 +0530

    Initial commit

---
+++ src/main/java/HDCF/HDFCPR.java
@@ -0,0 +1,29 @@
+package HDCF;
+import com.microsoft.playwright.*;
+
+public class HDFCPR {
+
+    public static void main(String[] args) {
+
+            try (Playwright playwright = Playwright.create()) {
+                Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false));
+                Page page = browser.newPage();
+                page.navigate("https://cpc_uat_admin.cormsquare.com/myLogin");
+                System.out.println(page.title());
+
+
+                String emailId = "Kaushik@Hdfcbank.com";
+                String password = "Admin@123";
+                page.locator("#mat-input-0").first().fill(emailId);
+                page.locator("#mat-input-1").first().fill(password);
+                page.locator("//button[contains(text(), 'Login')]").last().click();
+                System.out.println("ABC");
+
+
+
+
+        }
+    }
+}
+
+
